56ca3fda284c6d149ea366526599b5a5a4d36cc4
yz18710109979 <79241484@qq.com>
2019-08-08 17:08:07 +0800

    x

---
+++ design-jdk_8_sources/src/test/java/com/example/text/TestJdk8.java
@@ -0,0 +1,15 @@
+package com.example.text;
+
+import java.lang.reflect.Field;
+import java.util.HashMap;
+
+import org.junit.Test;
+
+public class TestJdk8 {
+
+	@Test
+	public void test1() throws Exception {
+		HashMap hashMap = new HashMap();
+	}
+	
+}
